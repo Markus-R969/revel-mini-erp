87f1e58fa71f422063684bb72ed1e4f1106c879b
markus-R969 <informaticaoliag@gmail.com>
2026-06-03 20:10:13 +0200

    Remove setup scripts (not part of production code)

--- test_db.py
+++
@@ -1,6 +0,0 @@
-﻿from app.db import engine, Base
-from app.models import Vehicle, Customer, Contract, Payment
-
-Base.metadata.create_all(bind=engine)
-print("✅" \
-" Todas las tablas creadas correctamente")
